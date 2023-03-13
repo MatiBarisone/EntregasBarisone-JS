0e6e1a7324b73a17ff8c8f7756b480e0f9334350
Mati Barisone <matibarisone@gmail.com>
2023-03-13 18:44:21 -0300

    Modificaciones Entrega Final

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
-//Clases
-class Producto {
-    constructor(nombre, precio) {
+// ================= CLASES =================
+class Producto{
+    constructor(img, id, nombre, precio){
+        this.img = img;
+        this.id = id;
         this.nombre = nombre;
         this.precio = precio;
     }
 }
 
-//Funciones
+class ProductoCarrito {
+    constructor(nombre, precio, cantidad = 1) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    sumarCantidad (){
+        this.cantidad++;
+    }
+}
+
+// ================= FUNCIONES =================
+
+/* Se encarga de renderizar todos los poductos en la página. */
 function renderizarProductos(productos){
     bodyProductos.innerHTML = "";
     productos.forEach(producto => {
@@ -35,7 +51,13 @@ function renderizarProductos(productos){
         boton.innerText= "Agregar al carrito"
 
         boton.addEventListener ("click", () => {
-            agregarProductos(producto);
+            agregarProductosAlCarro(producto);
+            Toastify({
+                text: "El objeto se ha agregado al Carrito",
+                position: "left",
+                duration: 1500,
+                backgroundColor: "green",
+                }).showToast();
         });
 
         cardBody.append (nombre);
@@ -52,6 +74,43 @@ function renderizarProductos(productos){
     });
 }
 
+/* Se encarga de obtener nuestros respectivos productos desde el Archivo .JSON (esto podría ser una futura API). */
+function obtenerProductosJSON(){
+    fetch('json/productos.json')
+    .then ((Response) => {
+        return Response.json();
+    })
+    .then ((productosJSON) => {
+        
+        for (const productoJSON of productosJSON){
+            listaDeProductos.push (new Producto (
+                productoJSON.img, 
+                productoJSON.id, 
+                productoJSON.nombre, 
+                productoJSON.precio))
+        }
+        
+        renderizarProductos(listaDeProductos);
+    })
+}
+
+/* Se encarga de Agregar un producto a nuestro carrito (esto se accionará cuando se toque el botón "Agregar al carrito"), 
+si se quiere agregar un producto que ya existe en el carrito la cantidad de ese producto aumentará. */
+function agregarProductosAlCarro(productoAgregado){
+    const indiceProductoExistente = carrito.findIndex( (productoExistente) => productoExistente.nombre.toLowerCase() === productoAgregado.nombre.toLowerCase());
+    if (indiceProductoExistente === -1){
+        carrito.push(new ProductoCarrito(productoAgregado.nombre, productoAgregado.precio));
+    } 
+    else{
+        carrito[indiceProductoExistente].sumarCantidad()
+    }
+
+    actualizarLS();
+    renderizarCarrito(carrito);
+    renderizarTotal(carrito);
+}
+
+/* Se encarga de renderizar todos los poductos que nosotros hayamos agregado a nuestro carrito. */
 function renderizarCarrito(productos){
     bodyTabla.innerHTML = "";
     productos.forEach(producto => {
@@ -63,18 +122,28 @@ function renderizarCarrito(productos){
         const tdPrecio = document.createElement("td");
         tdPrecio.innerHTML = `AR$ ${new Intl.NumberFormat('es-MX').format(producto.precio)}`;
 
+        const tdCantidad = document.createElement("td");
+        tdCantidad.innerHTML = `${producto.cantidad}`;
+
         const tdAcciones = document.createElement("td");
         const botonEliminarProducto = document.createElement("button");
         botonEliminarProducto.className = "btn btn-sm btn-outline-danger";
         botonEliminarProducto.innerText="Eliminar";
         botonEliminarProducto.addEventListener ("click", () => {
             eliminarProducto(producto);
+            Toastify({
+                text: "Los objetos se han eliminado del carrito",
+                position: "left",
+                duration: 1500,
+                backgroundColor: "red",
+                }).showToast();
         });
 
         tdAcciones.append(botonEliminarProducto);
 
         fila.append(tdNombre);
         fila.append(tdPrecio);
+        fila.append(tdCantidad);
         fila.append(tdAcciones);
 
         bodyTabla.append(fila);
@@ -82,127 +151,88 @@ function renderizarCarrito(productos){
     })
 }
 
+/* Se encarga de renderizar y calcular el Total de los productos que se encuentran en el carrito. */
 function renderizarTotal(productos){
     bodyTotal.innerHTML = "";
     let total = 0;
     const tdTotal = document.createElement("td");
     productos.forEach( producto => {
-        total += producto.precio;
+        total += (producto.precio * producto.cantidad);
     })
     tdTotal.innerHTML = `AR$ ${new Intl.NumberFormat('es-MX').format(total)}`;
     tdTotal.className = "fs-2"
     bodyTotal.append(tdTotal);
 }
 
-function agregarProductos(producto){
-    const nombre = producto.nombre;
-    const precio = producto.precio;
-
-    console.log (nombre);
-    console.log (precio);
-    
-    carrito.push(new Producto(nombre, precio));
-
-    actualizarLS();
-    renderizarCarrito(carrito);
-    renderizarTotal(carrito);
-}
-
-function eliminarProducto(producto){
-    const indiceElementoAEliminar = carrito.findIndex ( (productoAEliminar) => {
-        return productoAEliminar.nombre.toLowerCase() == producto.nombre.toLowerCase();
+/* Se encarga de Eliminar un producto de nuestro carrito (esto se accionará cuando se toque el botón "Eliminar"
+dentro de el carrito). Esto eliminará todas las instancias del producto en cuestión. */
+function eliminarProducto(productoAEliminar){
+    const indiceElementoAEliminar = carrito.findIndex ( (ProductoCarrito) => {
+        return ProductoCarrito.nombre.toLowerCase() == productoAEliminar.nombre.toLowerCase();
     });
 
-    carrito.splice(indiceElementoAEliminar, 1);
+    if (indiceElementoAEliminar !== -1){
+        carrito.splice(indiceElementoAEliminar, 1);
+    }
     
     actualizarLS();
     renderizarCarrito(carrito);
     renderizarTotal(carrito);
 }
 
+/* Se encarga de buscar si existe una instancia del Carrito en el Local Storage, y si es así lo cargará
+para mostrarlo en pantalla */
 function obtenerCarrito(){
-    let productos = [];
-    let prodcutosLS = localStorage.getItem("productos");
-    if (prodcutosLS !== null){
-        const productosJSON = JSON.parse(prodcutosLS);
+    let carro = [];
+    let carritoLS = localStorage.getItem("productos");
+    if (carritoLS !== null){
+        const productosJSON = JSON.parse(carritoLS);
         for (const productoJSON of productosJSON){
-            productos.push (new Producto (productoJSON.nombre, productoJSON.precio));
+            carro.push (new ProductoCarrito (productoJSON.nombre, productoJSON.precio, productoJSON.cantidad));
         }
     }
-    return productos;
+    return carro;
 }
 
+/* Se encarga de actualizar el Local Storage, cada vez que el carrito es modificado */
 function actualizarLS(){
     const carritoJSON = JSON.stringify(carrito);
     localStorage.setItem("productos", carritoJSON); 
 }
 
-//inicio Programa
-const listaDeProductos = [
-    {
-        img: "img/zapatilla7.svg",
-        id:1, 
-        nombre:"Converse Run Star Hike", 
-        precio:36890
-    },
-    {
-        img: "img/zapatilla1.svg",
-        id:4, 
-        nombre:"VERSACE TRIGRECA SNEAKERS", 
-        precio:105999
-    },
-    {
-        img: "img/zapatilla9.svg",
-        id:2, 
-        nombre:"Adidas Sply-350", 
-        precio:56580
-    },
-    {
-        img: "img/zapatilla8.svg",
-        id:3, 
-        nombre:"Halo Leather Sneaker", 
-        precio:100000
-    },
-    {
-        img: "img/zapatilla2.svg",
-        id:6, 
-        nombre:"Adidas x DragonBall Freezer Edition", 
-        precio:75500
-    },
-    {
-        img: "img/zapatilla6.svg",
-        id:9, 
-        nombre:'Air Jordan 1 "Origin Story"', 
-        precio:63699
-    },
-    {
-        img: "img/zapatilla3.svg",
-        id:7, 
-        nombre:"Adidas x DragonBall Son Gohan Edition", 
-        precio:80999
-    },
-    {
-        img: "img/zapatilla5.svg",
-        id:5, 
-        nombre:"Adidas x DragonBall Son Goku Edition", 
-        precio:75500
-    },
-    {
-        img: "img/zapatilla4.svg",
-        id:8, 
-        nombre:"Adidas x DragonBall Shenron Edition", 
-        precio:76600
-    },
-];
-
+// ================= INICIO PROGRAMA =================
+const listaDeProductos = [];
 const carrito = obtenerCarrito();
 const bodyProductos = document.getElementById("bodyProductos");
 const bodyTabla = document.getElementById("bodyTabla");
 const bodyTotal = document.getElementById("bodyTotal");
 const formularioBuscarProductos = document.getElementById("buscarProductos");
 const inputNombreBuscado = document.getElementById("nombreBuscado");
+const botonDeCompra = document.getElementById("botonCompra");
 
 
+botonDeCompra.addEventListener("click", (event) => {
+    carrito.length === 0 ? 
+    (Swal.fire({
+        title: 'Error 404!',
+        text: 'No se han encontrado productos en tu Carrito, debes agregar alguno para poder seguir con la compra!',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+    })) 
+    :(Swal.fire({
+        title: 'Compra Exitosa!',
+        text: 'Su compra se ha realizado con exito!',
+        icon: 'success',
+        confirmButtonText: 'Ok'
+    }).then((result) =>{
+        if (result.isConfirmed){
+            carrito.splice(0,carrito.length);
+            actualizarLS();
+            renderizarCarrito(carrito);
+            renderizarTotal(carrito);
+        }
+    }))
+})
 
 formularioBuscarProductos.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -219,7 +249,7 @@ formularioBuscarProductos.addEventListener("submit", (event) => {
     renderizarProductos(productosFiltrados);
 })
 
-
+obtenerProductosJSON();
 renderizarProductos(listaDeProductos);
 renderizarCarrito(carrito);
 renderizarTotal(carrito);
